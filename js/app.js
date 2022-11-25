c0ca0b81cd144b4784a212f8475c5835e5dbd6f5
jomoke814 <jcullen814@gmail.com>
2022-11-25 17:59:21 +0300

    changing card dynamic code

--- js/app.js
+++ js/app.js
@@ -2,19 +2,68 @@
 
 const projectCards = document.querySelector('.project-card');
 projects.forEach(project => {
-    projectCards.innerHTML += `
-    <div class="cards">
-                <div class="image">
-                    <img src="images/${project.image}" alt="design-img">
-                </div>
-                <div class="description">
-                    <h2>${project.name}</h2>
-                    <p>${project.description}</p>
-                    <div class="links">
-                        <a href="https://github.com/jomoke814/news-homepage">Solution</a>
-                        <a href="https://jomoke814.github.io/news-homepage/">Live</a>
-                    </div>
-                </div>
-            </div>
-    `;
+    const card = document.createElement('div');
+    card.classList.add('cards');
+    const image = document.createElement('div');
+    image.classList.add('image');
+    const img = document.createElement('img');
+    img.setAttribute('src', `${project.image}`)
+    const tags = document.createElement('div');
+    tags.classList.add('tags');
+    const para1 = document.createElement('p');
+    para1.textContent = `${project.tags.tag1}`;
+    const para2 = document.createElement('p');
+    para2.textContent = `${project.tags.tag2}`;
+    const buttonTag = document.createElement('button');
+    buttonTag.textContent = `${project.tags.tag3}`;
+    const descriptionTag = document.createElement('div');
+    descriptionTag.classList.add('description');
+    const heading = document.createElement('h2');
+    heading.textContent = `${project.title}`;
+    const para = document.createElement('p');
+    para.textContent = `${project.description}`;
+    const linkTag = document.createElement('div');
+    linkTag.classList.add('links');
+    const para3 = document.createElement('p')
+    para3.textContent = 
+
+
+    projectCards.appendChild(card);
+    card.appendChild(image);
+    image.appendChild(img);
+    image.appendChild(tags);
+    tags.appendChild(para1);
+    tags.appendChild(para2);
+    tags.appendChild(buttonTag);
+    card.appendChild(descriptionTag);
+    descriptionTag.appendChild(heading);
+    descriptionTag.appendChild(para);
+    descriptionTag.appendChild(linkTag);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div class="cards">
+    //             <div class="image">
+    //                 <img src="images/${project.image}" alt="design-img">
+    //             </div>
+    //             <div class="description">
+    //                 <h2>${project.name}</h2>
+    //                 <p>${project.description}</p>
+    //                 <div class="links">
+    //                     <a href="https://github.com/jomoke814/news-homepage">Solution</a>
+    //                     <a href="https://jomoke814.github.io/news-homepage/">Live</a>
+    //                 </div>
+    //             </div>
+    //         </div>
--- js/projects.js
+++ js/projects.js
@@ -1,57 +1,156 @@
 let projects = [
     {
-        image: "desktop-design-2.jpg",
-        name: "News Homepage",
+        image: "images/desktop-design-2.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "News Homepage",
         description: "This is News homepage designed and its built using Css grid and flexbox skills. Its was a perfect project to flex css grid skills.",
+        link: {
+            link1: '',
+            link2: ''
+        }
     },
     {
-        image: "desktop-design.jpg",
-        name: "Stats Preview Card Component",
+        image: "images/desktop-design.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "Stats Preview Card Component",
         description: "This is a great small challenge to help get you used to building to a design. Stats preview card component designed and Its built using flexbox and Css grid skills.",
+        link: {
+            link1: '',
+            link2: ''
+        }
     },
     {
-        image: "desktop-design-3.jpg",
-        name: "Profile Card Component",
+        image: "images/desktop-design-3.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "Profile Card Component",
         description: "This is profile card component designed and Its built using felxbox and Css grid skills. The card layout doesn't shift, so it's also great for those that haven't dived into responsive websites yet!",
+        link: {
+            link1: '',
+            link2: ''
+        }
     },
     {
-        image: "desktop-design-4.jpg",
-        name: "Product Preview Card Component",
+        image: "images/desktop-design-4.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "Product Preview Card Component",
         description: "This is Product preview card component designed and Its built using Css grid skills. The responsive design was a bit challenging.",
+        link: {
+            link1: '',
+            link2: ''
+        }
     },
     {
-        image: "desktop-design-5.jpg",
-        name: "NFT Preview Card Component",
+        image: "images/desktop-design-5.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "NFT Preview Card Component",
         description: "This HTML & CSS only challenge is perfect for anyone just starting out or anyone wanting a small project to play around with.",
+        link: {
+            link1: '',
+            link2: ''
+        }
     },
     {
-        image: "desktop-design-6.jpg",
-        name: "Fylo Data Storage Component",
+        image: "images/desktop-design-6.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "Fylo Data Storage Component",
         description: "This component has some interesting CSS challenges in the design. If you're looking to test your CSS skills, this will be a great project for you.",
+        link: {
+            link1: '',
+            link2: ''
+        }
     },
     {
-        image: "desktop-design-7.jpg",
-        name: "Four Card Feature Section",
+        image: "images/desktop-design-7.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "Four Card Feature Section",
         description: "A nice layout-based challenge for beginners. This will test anyone who is new to multi-column and responsive layouts.",
+        link: {
+            link1: '',
+            link2: ''
+        }
     },
     {
-        image: "desktop-design-8.jpg",
-        name: "3-Column Preview Card Component",
+        image: "images/desktop-design-8.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "3-Column Preview Card Component",
         description: "This challenge is perfect if you're just getting started. The shift between the layouts will be a nice test if you're new to building responsive projects.",
+        link: {
+            link1: '',
+            link2: ''
+        }
     },
     {
-        image: "desktop-preview.jpg",
-        name: "Order Summary Component",
+        image: "images/desktop-preview.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "Order Summary Component",
         description: "A perfect project for newbies who are starting to build confidence with layouts. Create this using flexbox and grid.",
+        link: {
+            link1: '',
+            link2: ''
+        }
     },
     {
-        image: "desktop-design-9.jpg",
-        name: "Testimonial grid section",
+        image: "images/desktop-design-9.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "Testimonial grid section",
         description: "This challenge will be perfect practice for anyone wanting to test their CSS Grid skills. Grid is such a powerful addition to CSS, so it's worth getting to grips with it!",
+        link: {
+            link1: '',
+            link2: ''
+        }
     },
     {
-        image: "desktop-design-10.jpg",
-        name: "QR code component",
+        image: "images/desktop-design-10.jpg",
+        tags: {
+            tag1: 'HTML',
+            tag2: 'CSS',
+            tag3: 'Newbie'
+        },
+        title: "QR code component",
         description: "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
+        link: {
+            link1: '',
+            link2: ''
+        }
     }
 ]
