dff8c185aa167441f17ef1014b81c1498bd409f2
jomoke814 <jcullen814@gmail.com>
2022-11-26 12:28:09 +0300

    Removing unncessary comments

--- js/app.js
+++ js/app.js
@@ -46,30 +46,3 @@ projects.forEach(project => {
     linkTag.appendChild(a);
     linkTag.appendChild(a2);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // <div class="cards">
-    //             <div class="image">
-    //                 <img src="images/${project.image}" alt="design-img">
-    //             </div>
-    //             <div class="description">
-    //                 <h2>${project.name}</h2>
-    //                 <p>${project.description}</p>
-    //                 <div class="links">
-    //                     <a href="https://github.com/jomoke814/news-homepage">Solution</a>
-    //                     <a href="https://jomoke814.github.io/news-homepage/">Live</a>
-    //                 </div>
-    //             </div>
-    //         </div>
